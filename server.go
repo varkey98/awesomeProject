5932f649ccf7a139851a6779c60911c8fbfec69e
Varkeychan Jacob <varkey98@gmail.com>
2025-05-27 15:22:49 +0530

    initial commit

---
+++ server.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Traceableai/goagent"
+	"github.com/Traceableai/goagent/config"
+	"github.com/gorilla/mux"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func startHttpServer() {
+	fmt.Println("Starting app on port 8080")
+	cfg := config.LoadFromFile("./config.yaml")
+
+	closer := goagent.Init(cfg)
+	defer closer()
+
+	r := mux.NewRouter()
+	r.Handle("/foo",
+		http.HandlerFunc(fooHandler))
+	//r.Handle("/extcap", traceablehttp.NewHandler(
+	//	http.HandlerFunc(extCapHandler), "/extcap"))
+	//r.Handle("/v1/traces", traceablehttp.NewHandler(
+	//	http.HandlerFunc(otlpHttpHandler), "/v1/traces"))
+	// Using log.Fatal(http.ListenAndServe(":8081", r)) causes a gosec timeout error.
+	// G114 (CWE-676): Use of net/http serve function that has no support for setting timeouts (Confidence: HIGH, Severity: MEDIUM)
+	srv := http.Server{
+		Addr:              ":8080",
+		Handler:           r,
+		ReadTimeout:       60 * time.Second,
+		WriteTimeout:      60 * time.Second,
+		ReadHeaderTimeout: 60 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+type Person struct {
+	Name      string   `json:"name"`
+	Telephone int      `json:"telephone"`
+	City      string   `json:"curr_city"`
+	Weather   string   `json:"weather"`
+	Nested    []Person `json:"nested"`
+}
+
+type Request struct {
+	Data []Person `json:"data"`
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	sBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("here")
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(r.Body)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(sBody)
+}
